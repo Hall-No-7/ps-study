7cfdbc43ea5b9033de74326bec49ac9fa8c4c81b
leejh7 <777joonho@kookmin.ac.kr>
2023-06-29 19:58:59 +0900

    feature: First commit

---
+++ dalcom/16197/16197.py
@@ -0,0 +1,2 @@
+from sys import stdin as input
+input = open("/Volumes/Program/ps-study/dalcom/16197/input.txt", "rt")
---
+++ dalcom/makeProb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# != 1 ]
+then
+	echo '문제 번호 파라미터로 넘길것!'
+	exit
+fi
+
+cd /Volumes/Program/ps-study/dalcom
+
+if [ -d $1 ]
+then 
+	echo '이미 존재하는 폴더임!'
+	exit
+fi
+
+mkdir $1
+touch $1/$1.py $1/input.txt
+
+echo -e "from sys import stdin as input\ninput = open(\"/Volumes/Program/ps-study/dalcom/$1/input.txt\", \"rt\")" > $1/$1.py
+
+echo '완료!' 
