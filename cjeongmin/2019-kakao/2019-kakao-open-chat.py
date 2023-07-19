d7177086cdb551d56ac59b83decc21589dd847b5
Choi Jeongmin <cjeongmin27@gmail.com>
2023-07-19 19:16:18 +0900

    Update 2019-kakao-open-chat.py

--- cjeongmin/2019-kakao/2019-kakao-open-chat.py
+++ cjeongmin/2019-kakao/2019-kakao-open-chat.py
@@ -3,17 +3,11 @@ def solution(record):
     users = {}
     for log in record:
         tokens = log.split()
-        if tokens[0] == 'Enter':
+        if tokens[0] == "Enter":
             users[tokens[1]] = tokens[2]
-            answer.append(f'#{tokens[1]}#님이 들어왔습니다.')
-        elif tokens[0] == 'Leave':
-            answer.append(f'#{tokens[1]}#님이 나갔습니다.')
+            answer.append((tokens[1], "님이 들어왔습니다."))
+        elif tokens[0] == "Leave":
+            answer.append((tokens[1], "님이 나갔습니다."))
         else:
             users[tokens[1]] = tokens[2]
-    
-    for i, log in enumerate(answer):
-        a = 0
-        b = log[1:].find('#')
-        userId = log[1:b+1]
-        answer[i] = log.replace(f'#{userId}#', users[userId])
-    return answer
+    return list(map(lambda x: "".join([users[x[0]], x[1]]), answer))
