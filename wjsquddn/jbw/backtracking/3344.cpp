6140f4703467fd5f2c054c8920ede29dad885e78
wjsquddn <wjsquddn@hanmail.net>
2023-07-19 13:39:07 +0900

    backtracking

---
+++ wjsquddn/jbw/backtracking/3344.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+using namespace std;
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n % 6 == 2) {
+        for (int i = 1; i <= (n / 2); i++) {
+            printf("%d\n", 2 * i);
+        }
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                if (i == 1) continue;
+                if (i == 3) {
+                    printf("%d\n", i);
+                    printf("%d\n", i - 2);
+                    continue;
+                }
+                if (i == 5) continue;
+
+                printf("%d\n", i);
+            }
+        }
+        printf("%d\n", 5);
+    } else if (n % 6 == 3) {
+        for (int i = 1; i <= (n / 2); i++) {
+            if (i == 1) continue;
+            if (i == n / 2) {
+                printf("%d\n", i * 2);
+                printf("%d\n", 2);
+                continue;
+            }
+            printf("%d\n", i * 2);
+        }
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                if (i == 1 || i == 3) {
+                    continue;
+                }
+                printf("%d\n", i);
+            }
+        }
+
+        printf("%d\n", 1);
+
+        printf("%d\n", 3);
+    } else {
+        for (int i = 1; i <= (n / 2); i++) {
+            printf("%d\n", i * 2);
+        }
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                printf("%d\n", i);
+            }
+        }
+    }
+}
