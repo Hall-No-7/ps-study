1a675c54c78b6ce2f2fcd3a082bca049494ae312
Choi Jeongmin <cjeongmin27@gmail.com>
2023-07-26 23:18:02 +0900

    2020-kakao-string-compression.py

---
+++ cjeongmin/2020-kakao/2020-kakao-string-compression.py
@@ -0,0 +1,16 @@
+def compression(s, length):
+    ret = ""
+    cnt = 1
+    pattern = s[:length]
+    for i in range(length, len(s), length):
+        if pattern == s[i:i+length]:
+            cnt += 1
+        else:
+            ret += f"{cnt if cnt > 1 else ''}{pattern}"
+            cnt = 1
+            pattern = s[i:i+length]
+    return ret + f"{cnt if cnt > 1 else ''}{pattern}"
+
+
+def solution(s):
+    return min(map(lambda x: len(compression(s, x)), range(1, len(s) // 2 + 1)))
