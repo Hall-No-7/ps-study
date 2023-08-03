0c3463b3167681206d24911a8eff2e956b9dfcb8
오홍석 <lkl4502@kookmin.ac.kr>
2023-08-03 23:26:43 +0900

    2020 kakao wall check

--- lkl4502/2020_kakao/wall_check_lv3.cpp
+++ lkl4502/2020_kakao/wall_check_lv3.cpp
@@ -7,40 +7,50 @@
     cout.tie(NULL);
 using namespace std;
 
+/* dist로 만들 수 있는 모든 순열을 만들어서 전부 구한다.*/
+
 int main() {
     fast_io;
-    int len = 200, answer = 0;
+    int len = 200, answer = -1;
     vector<int> weak = {0, 10, 50, 80, 120, 160}, dist = {1, 10, 5, 40, 30};
-    sort(dist.begin(), dist.end(), greater<int>());
-    while (dist.size() != 0) { // 다 투입해도 못하면 -1을 리턴 해야하므로
-        int workMan = dist[0];
-        if (workMan >= len) {
-            answer++;
-            break;
-        }
-        int maxCnt = -1, start;
-        for (int i = 0; i < weak.size(); i++) {
-            int cnt = upper_bound(weak.begin(), weak.end(),
-                                  (workMan + weak[i]) % len) -
-                      weak.begin() - i;
-            if ((workMan + weak[i]) % len < weak[i])
-                cnt += weak.size();
-            start = (maxCnt < cnt) ? weak[i] : start;
-            maxCnt = max(maxCnt, cnt);
-        }
+    sort(dist.begin(), dist.end());
 
-        dist.erase(dist.begin());
-        auto it = find(weak.begin(), weak.end(), start);
-        for (int i = 0; i < maxCnt; i++) {
-            weak.erase(it);
-            if (it == weak.end())
-                it = weak.begin();
+    do {
+        vector<int> tmp_weak = weak, tmp_dist = dist;
+        int cnt = 0;
+        while (tmp_dist.size() != 0) {
+            int workMan = tmp_dist[0];
+            if (workMan >= len) {
+                cnt++;
+                break;
+            }
+            int maxCnt = -1, start;
+            for (int i = 0; i < tmp_weak.size(); i++) {
+                int cnt = upper_bound(tmp_weak.begin(), tmp_weak.end(),
+                                      (workMan + tmp_weak[i]) % len) -
+                          tmp_weak.begin() - i;
+                if ((workMan + tmp_weak[i]) % len < tmp_weak[i])
+                    cnt += tmp_weak.size();
+                start = (maxCnt < cnt) ? tmp_weak[i] : start;
+                maxCnt = max(maxCnt, cnt);
+            }
+            tmp_dist.erase(tmp_dist.begin());
+            auto it = find(tmp_weak.begin(), tmp_weak.end(), start);
+            for (int i = 0; i < maxCnt; i++) {
+                tmp_weak.erase(it);
+                if (it == tmp_weak.end())
+                    it = tmp_weak.begin();
+            }
+            cnt++;
+            if (tmp_weak.size() == 0)
+                break;
         }
-        answer++;
-        if (weak.size() == 0)
-            break;
-    }
-    if (weak.size() != 0)
-        answer = -1;
+        if (tmp_weak.size() != 0)
+            cnt = -1;
+        if (cnt == -1)
+            continue;
+        int tmp = (answer == -1) ? INT32_MAX : answer;
+        answer = min(tmp, cnt);
+    } while (next_permutation(dist.begin(), dist.end()));
     cout << answer << endl;
 }
