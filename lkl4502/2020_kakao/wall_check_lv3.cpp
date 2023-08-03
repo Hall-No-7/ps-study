7980a780c2537e636c8573f866bca3c63385228b
오홍석 <lkl4502@kookmin.ac.kr>
2023-08-03 22:37:37 +0900

    2020 kakao Wall Check fail #1

---
+++ lkl4502/2020_kakao/wall_check_lv3.cpp
@@ -0,0 +1,46 @@
+// By Hongs
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fast_io                                                                \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(NULL);                                                             \
+    cout.tie(NULL);
+using namespace std;
+
+int main() {
+    fast_io;
+    int len = 200, answer = 0;
+    vector<int> weak = {0, 10, 50, 80, 120, 160}, dist = {1, 10, 5, 40, 30};
+    sort(dist.begin(), dist.end(), greater<int>());
+    while (dist.size() != 0) { // 다 투입해도 못하면 -1을 리턴 해야하므로
+        int workMan = dist[0];
+        if (workMan >= len) {
+            answer++;
+            break;
+        }
+        int maxCnt = -1, start;
+        for (int i = 0; i < weak.size(); i++) {
+            int cnt = upper_bound(weak.begin(), weak.end(),
+                                  (workMan + weak[i]) % len) -
+                      weak.begin() - i;
+            if ((workMan + weak[i]) % len < weak[i])
+                cnt += weak.size();
+            start = (maxCnt < cnt) ? weak[i] : start;
+            maxCnt = max(maxCnt, cnt);
+        }
+
+        dist.erase(dist.begin());
+        auto it = find(weak.begin(), weak.end(), start);
+        for (int i = 0; i < maxCnt; i++) {
+            weak.erase(it);
+            if (it == weak.end())
+                it = weak.begin();
+        }
+        answer++;
+        if (weak.size() == 0)
+            break;
+    }
+    if (weak.size() != 0)
+        answer = -1;
+    cout << answer << endl;
+}
