134fd5cdcb6a4ea8d18a24113b5f14b89d1d55f9
오홍석 <lkl4502@kookmin.ac.kr>
2023-08-03 19:31:42 +0900

    2020 kakao move block fail #2

--- lkl4502/2020_kakao/move_block_lv3.cpp
+++ lkl4502/2020_kakao/move_block_lv3.cpp
@@ -1,3 +1,4 @@
+// By Hongs
 #include <bits/stdc++.h>
 #define endl "\n"
 #define fast_io                                                                \
@@ -6,35 +7,30 @@
     cout.tie(NULL);
 using namespace std;
 
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {-1, 1, 0, 0};
+int dx[4] = {0, 0, -1, 1}; // row
+int dy[4] = {-1, 1, 0, 0}; // col
 
 int rotateX[2][4] = {{0, 1, 0, 1}, {-1, -1, 0, 0}}; // rotateX[0] vertical
-int rotateY[2][4] = {{-1, -1, 0, 0}, {0, 1, 0, 1}}; // rotateY[0] 은 vertical
+int rotateY[2][4] = {{-1, -1, 0, 0}, {0, 1, 0, 1}}; // rotateY[0] vertical
 
 int answer = INT32_MAX, goal;
 
-bool visited[100][100] = {
-    {false},
+bool visited[2][100][100] = {
+    {
+        {false},
+    },
 };
 
 bool CheckingMove(vector<vector<int>> &board, int row, int col, bool vertical) {
     if (vertical) { // 수직
         if (row < 0 || col < 0 || row > goal || col > goal || row + 1 > goal ||
-            board[row][col] == 1 || board[row + 1][col] == 1)
+            board[row][col] != 0 || board[row + 1][col] != 0)
             return false;
     } else { // 가로
         if (row < 0 || col < 0 || row > goal || col > goal || col + 1 > goal ||
-            board[row][col] == 1 || board[row][col + 1] == 1)
+            board[row][col] != 0 || board[row][col + 1] != 0)
             return false;
     }
-    return true;
-}
-
-bool CheckingRotate(vector<vector<int>> &board, int row, int col,
-                    bool vertical) {
-    if (row < 0 || col < 0 || row > goal || col > goal || board[row][col] == 1)
-        return false;
     return true;
 }
 
@@ -51,70 +47,61 @@ void BackTracking(vector<vector<int>> &board, pair<int, int> &drone, int cnt,
             return;
         }
     }
-
+    int type = (vertical) ? 1 : 0;
     for (int i = 0; i < 4; i++) {
         int newRow = drone.first + dx[i];
         int newCol = drone.second + dy[i];
         if (CheckingMove(board, newRow, newCol, vertical)) {
-            if (!visited[newRow][newCol]) {
-                visited[newRow][newCol] = true;
-                cnt++, drone.first += dx[i], drone.second += dy[i];
-                BackTracking(board, drone, cnt, vertical);
-                visited[newRow][newCol] = false;
-                cnt--, drone.first -= dx[i], drone.second -= dy[i];
+            if (!visited[type][newRow][newCol]) {
+                visited[type][newRow][newCol] = true;
+                drone.first += dx[i], drone.second += dy[i];
+                BackTracking(board, drone, cnt + 1, vertical);
+                visited[type][newRow][newCol] = false;
+                drone.first -= dx[i], drone.second -= dy[i];
             }
         }
     }
 
     for (int i = 0; i < 4; i++) { // 회전식에 인덱스 조정을 어떤 식으로 해야하고
                                   // 검사는 어떤 식으로 해야하는지 고민할 것
-        int newRow, newCol;
-        int type = (vertical) ? 0 : 1;
+        if (i < 2 && !vertical)
+            continue;
+        else if (i > 2 && vertical)
+            continue;
+
+        int newRow = drone.first + dx[i];
+        int newCol = drone.second + dy[i];
 
         if (vertical) {
-            if (i < 2) {
-                if (!CheckingRotate(board, drone.first, drone.second + 1,
-                                    vertical) ||
-                    !CheckingRotate(board, drone.first + 1, drone.second + 1,
-                                    vertical))
-                    continue;
-            } else {
-                if (!CheckingRotate(board, drone.first, drone.second - 1,
-                                    vertical) ||
-                    !CheckingRotate(board, drone.first + 1, drone.second - 1,
-                                    vertical))
-                    continue;
+            if (CheckingMove(board, newRow, newCol, vertical)) {
+                for (int j = 2 * i; j < 2 * (i + 1); j++) {
+                    if (!visited[type][drone.first + rotateX[0][j]]
+                                [drone.second + rotateY[0][j]]) {
+                        drone.first += rotateX[0][j],
+                            drone.second += rotateY[0][j];
+                        visited[type][drone.first][drone.second] = true;
+                        BackTracking(board, drone, cnt + 1, !vertical);
+                        visited[type][drone.first][drone.second] = false;
+                        drone.first -= rotateX[0][j],
+                            drone.second -= rotateY[0][j];
+                    }
+                }
             }
-            newRow = drone.first + rotateX[0][i];
-            newCol = drone.second + rotateY[0][i];
         } else {
-            if (i < 2) {
-                if (!CheckingRotate(board, drone.first - 1, drone.second,
-                                    vertical) ||
-                    !CheckingRotate(board, drone.first - 1, drone.second + 1,
-                                    vertical))
-                    continue;
-            } else {
-                if (!CheckingRotate(board, drone.first + 1, drone.second,
-                                    vertical) ||
-                    !CheckingRotate(board, drone.first + 1, drone.second + 1,
-                                    vertical))
-                    continue;
+            if (CheckingMove(board, newRow, newCol, vertical)) {
+                for (int j = 2 * (i - 2); j < 2 * (i - 1); j++) {
+                    if (!visited[type][drone.first + rotateX[1][j]]
+                                [drone.second + rotateY[1][j]]) {
+                        drone.first += rotateX[1][j],
+                            drone.second += rotateY[1][j];
+                        visited[type][drone.first][drone.second] = true;
+                        BackTracking(board, drone, cnt + 1, !vertical);
+                        visited[type][drone.first][drone.second] = false;
+                        drone.first -= rotateX[1][j],
+                            drone.second -= rotateY[1][j];
+                    }
+                }
             }
-            newRow = drone.first + rotateX[1][i];
-            newCol = drone.second + rotateY[1][i];
-        }
-
-        if (!visited[newRow][newCol]) {
-            visited[newRow][newCol] = true;
-            vertical = (vertical) ? false : true;
-            cnt++, drone.first += rotateX[type][i],
-                drone.second += rotateY[type][i];
-            BackTracking(board, drone, cnt, vertical);
-            visited[newRow][newCol] = false;
-            vertical = (vertical) ? false : true;
-            cnt--, drone.first -= rotateX[type][i],
-                drone.second -= rotateY[type][i];
         }
     }
 }
@@ -128,7 +115,7 @@ int main() {
                                  {0, 0, 0, 0, 0}};
     pair<int, int> drone = {0, 0};
     goal = board.size() - 1;
-    visited[0][0] = true;
-    BackTracking(board, drone, 0, 0);
+    visited[0][0][0] = true;
+    BackTracking(board, drone, 0, false);
     cout << answer << endl;
 }
